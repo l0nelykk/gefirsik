6fa5d1dcb6dcc15890c9a6dd9aecad17772b930d
l0nelykk <cvamiross408@gmail.com>
2026-06-29 11:35:35 +0300

    did the coder file, there will be all operations with code(execution, architecture of commands directory)

---
+++ coder.py
@@ -0,0 +1,12 @@
+class coder:
+    def __init__():
+        pass
+    
+    def creating(string): #return gemini response
+        pass
+
+    def writing(): #write to file and make arhitecture
+        pass
+    #execution of created code
+    def execution(): #argument is creating return
+        pass #there should be checking of code
